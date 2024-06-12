17cd059df2de4cb86dc85bd07da01843b5c22312
raghveerdeol <deolraghveer5@gmail.com>
2024-06-12 15:57:18 +0200

    aggiunto nav nel header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+data() {
+return {
+
+}
+}
+}
+</script>
+
+<template>
+    <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid ">
+        <a class="navbar-brand" href="#">
+        <img src="https://i.pinimg.com/736x/f4/47/f1/f447f11c28b97952593fd45b4f678c8b.jpg" alt="Logo" width="100" height="40" class="d-inline-block align-text-top">
+        <h1 class="d-inline align-middle" >yu-gi-oh api</h1>
+        </a>
+    </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+    h1{
+        text-transform: uppercase;
+    }
+</style>
